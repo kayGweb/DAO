1788cd5b5c54402a60a9adc2a7bc99b8da5b743a
Ideal Kendall <antonio.kendall@gmail.com>
2024-04-05 00:55:21 -0400

    Completed Dao proposal Creation and Voting

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -8,6 +8,28 @@ contract DAO {
     address owner;
     Token public token;
     uint256 public quorum;
+    uint256 public proposalCount;
+
+    struct Proposal {
+        uint256 id;
+        string name;
+        uint256 amount;
+        address payable recipient;
+        uint256 votes;
+        bool finalized;
+    }
+
+    event Propose(
+        uint256 id,
+        uint256 amount,
+        address recipient,
+        address creator
+    );
+
+    event Vote(uint256 id, address investor);
+
+    mapping(uint256 => Proposal) public proposals;
+    mapping(address => mapping(uint256 => bool)) votes;
 
     constructor(Token _token, uint256 _quorum) {
         owner = msg.sender;
@@ -17,4 +39,48 @@ contract DAO {
 
     // Allow contract to receive ether
     receive() external payable {}
+
+    modifier InvestorOnly() {
+        require(token.balanceOf(msg.sender) > 0, "Must be token holder");
+        _;
+    }
+
+    function createProposal(
+        string memory _name,
+        uint256 _amount,
+        address payable _recipient
+    ) external InvestorOnly {
+        // address(this).balance is the balance of the contract
+        require(address(this).balance >= _amount, "Insufficient balance");
+
+        proposalCount++;
+        // Save proposal to mapping
+        proposals[proposalCount] = Proposal(
+            proposalCount,
+            _name,
+            _amount,
+            _recipient,
+            0,
+            false
+        );
+
+        emit Propose(proposalCount, _amount, _recipient, msg.sender);
+    }
+
+    function vote(uint256 _id) external InvestorOnly {
+        //fetch proposal from mappig by id
+        Proposal storage proposal = proposals[_id];
+
+        // Dont let invester vote twice
+        require(!votes[msg.sender][_id], "Investor can only vote once");
+
+        //update votes
+        proposal.votes += token.balanceOf(msg.sender);
+
+        //track that use has voted
+        votes[msg.sender][_id] = true;
+
+        //emit and event
+        emit Vote(_id, msg.sender);
+    }
 }
--- test/DAO.js
+++ test/DAO.js
@@ -10,20 +10,44 @@ const ether = tokens;
 describe("DAO", () => {
 	let token, dao, deployer, funder;
 	let quorum = "500000000000000000000001";
+	let investor1, investor2, investor3, investor4, investor5, recipient, user;
+	let transaction, result;
 
 	beforeEach(async () => {
-		// Deploy Token
-		const Token = await ethers.getContractFactory("Token");
-		token = await Token.deploy("Dapp University", "DAPP", "1000000");
-
-		//Deploy DAO
-		const DAO = await ethers.getContractFactory("DAO");
-		dao = await DAO.deploy(token.address, quorum);
-
 		//Setup Accounts
 		const accounts = await ethers.getSigners();
 		deployer = accounts[0];
 		funder = accounts[1];
+		investor1 = accounts[2];
+		investor2 = accounts[3];
+		investor3 = accounts[4];
+		investor4 = accounts[5];
+		investor5 = accounts[6];
+		recipient = accounts[7];
+		user = accounts[8];
+
+		// Deploy Token
+		const Token = await ethers.getContractFactory("Token");
+		token = await Token.deploy("Dapp University", "DAPP", "1000000");
+
+		transaction = await token.connect(deployer).transfer(investor1.address, tokens(200000));
+		await transaction.wait();
+
+		transaction = await token.connect(deployer).transfer(investor2.address, tokens(200000));
+		await transaction.wait();
+
+		transaction = await token.connect(deployer).transfer(investor3.address, tokens(200000));
+		await transaction.wait();
+
+		transaction = await token.connect(deployer).transfer(investor4.address, tokens(200000));
+		await transaction.wait();
+
+		transaction = await token.connect(deployer).transfer(investor5.address, tokens(200000));
+		await transaction.wait();
+
+		//Deploy DAO
+		const DAO = await ethers.getContractFactory("DAO");
+		dao = await DAO.deploy(token.address, quorum);
 
 		// Funder sends 100 Ether to DAO treasury for Governace
 		await funder.sendTransaction({
@@ -43,6 +67,84 @@ describe("DAO", () => {
 
 		it("returns quorum", async () => {
 			expect(await dao.quorum()).to.equal(quorum);
+		});
+	});
+
+	describe("Proposal Creation", () => {
+		describe("Success", () => {
+			beforeEach(async () => {
+				transaction = await dao.connect(investor1).createProposal("First Proposal", ether(100), recipient.address);
+				result = await transaction.wait();
+			});
+
+			it("updates proposal count", async () => {
+				expect(await dao.proposalCount()).to.equal(1);
+			});
+
+			it("updates proposal mapping", async () => {
+				const proposal = await dao.proposals(1);
+				expect(proposal.id).to.eq(1);
+				expect(proposal.amount).to.eq(ether(100));
+				expect(proposal.recipient).to.eq(recipient.address);
+			});
+
+			it("emits propose event", async () => {
+				//expect(result.events[0].event).to.eq("Propose");
+				//console.log(result.events[0].event);
+				await expect(transaction).to.emit(dao, "Propose").withArgs(1, ether(100), recipient.address, investor1.address);
+			});
+		});
+
+		describe("Failure", () => {
+			it("rejects insufficent funds", async () => {
+				await expect(dao.connect(investor1).createProposal("First Proposal", ether(1000), recipient.address)).to.be.revertedWith("Insufficient balance");
+			});
+
+			it("reject non-investor", async () => {
+				expect(await dao.connect(investor1).createProposal("First Proposal", ether(100), recipient.address)).to.be.revertedWith("Must be token holder");
+			});
+
+			it("checks for quorum", async () => {});
+		});
+	});
+
+	describe("Voting", () => {
+		let transaction, result;
+
+		beforeEach(async () => {
+			transaction = await dao.connect(investor1).createProposal("First Proposal", ether(100), recipient.address);
+			result = await transaction.wait();
+		});
+
+		describe("Success", () => {
+			beforeEach(async () => {
+				transaction = await dao.connect(investor1).vote(1);
+				result = await transaction.wait();
+			});
+
+			it("updates vote count", async () => {
+				const proposal = await dao.proposals(1);
+				expect(proposal.votes).to.eq(tokens(200000));
+			});
+
+			it("emits vote event", async () => {
+				await expect(transaction).to.emit(dao, "Vote").withArgs(1, investor1.address);
+			});
+		});
+
+		describe("Failure", () => {
+			it("rejects non-invester", async () => {
+				await expect(dao.connect(user).vote(1)).to.be.reverted;
+			});
+
+			it("reject double voting", async () => {
+				transaction = await dao.connect(investor1).vote(1);
+				await transaction.wait();
+
+				await expect(dao.connect(investor1).vote(1)).to.be.reverted;
+			});
+
+			//it("checks for quorum", async () => {});
 		});
 	});
 });
